7b1e2778e2217e8fd4a8217d315e92729018d13a
Tarek Ali <tarekali15@outlook.com>
2019-11-26 20:55:27 -0600

    working on db features

--- app.py
+++ app.py
@@ -3,25 +3,26 @@ import sqlite3
 
 app = Flask(__name__)
 
-try:
-    db = sqlite3.connect('accounts.sqlite')
-except:
-    print('database error')
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/loggedin', methods=["POST"])
+@app.route('/loggedin', methods=["GET, POST"])
 def verify_login():
     username = request.form.get('username')
     password = request.form.get('password')
     if username and password:
+        try:
+            db = sqlite3.connect('accounts.sqlite')
+        except:
+            print('database error')
         query = db.execute('SELECT * FROM ACCOUNT;')
         if username in query and password in query:
             return render_template('survey.html')
+    else:
+        return 'please enter a username AND password'
 
 
 '''
