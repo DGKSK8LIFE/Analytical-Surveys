578e60960936b76d1e24df8fede8e65f440673f3
Tarek Ali <tarekali15@outlook.com>
2019-11-27 13:32:21 -0600

    another naming error in createaccount.html :facepalm:

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def create_page():
     return render_template('createaccount.html')
 
 
-@app.route('/createaccount', methods=["POST"])
+@app.route('/submitcreatepage', methods=["POST"])
 def create_account():
     username = request.form.get('username')
     password = request.form.get('password')
