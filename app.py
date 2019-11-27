8bdf566fe462b03adf1f537bea48d031e8da5183
Tarek Ali <tarekali15@outlook.com>
2019-11-26 19:32:05 -0600

    working on the backend

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/loggedin', methods=["POST"])
+def verify_login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    return username, password
+
+
 @app.route('/createaccount.html')
-def create_account():
+def create_page():
     return render_template('createaccount.html')
