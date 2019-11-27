aa499e66b43561d8b552e29f3d4c7b3aa2fc1b53
Tarek Ali <tarekali15@outlook.com>
2019-11-26 19:55:18 -0600

    initial login feature implemented (pre-test)

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+try:
+    db = sqlite3.connect('accounts.sqlite')
+except:
+    print('database error')
 
 
 @app.route('/')
@@ -12,7 +18,10 @@ def index():
 def verify_login():
     username = request.form.get('username')
     password = request.form.get('password')
-    return 'lmao'
+    if username and password:
+        query = db.execute('SELECT * FROM ACCOUNT;')
+        if username in query and password in query:
+            return render_template('survey.html')
 
 
 '''
